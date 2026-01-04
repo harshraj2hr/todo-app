a57a0ce7c3a828a43ff18f5755fb38364e8acddb
harsh <harshraj2hr1408@gmail.com>
2026-01-04 20:29:17 +0530

    initial commit, pushed basic functionalities

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [todo, setToDo] = useState("");
+  const [todoList, setToDoList] = useState([]);
+  const [editTodoId, setEditToDo] = useState(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(todo.trim() === "") return;
+    if(editTodoId !== null){
+      todoList.map((item) => {
+        if(item.id === editTodoId){
+          item.todo = todo;
+        }
+        return item;
+      });
+      setToDoList(todoList);
+      setEditToDo(null);
+      setToDo("");
+      return;
+    }
+
+    setToDoList([...todoList, {id:`todo+${Date.now()}`, todo}]);
+    setToDo("");
+  }
+
+  const handleDelete = (id) => {
+    if(id === "") return;
+    console.log("deleting item with id:", id);
+    setToDoList(todoList.filter((item) => item.id !== id));
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <h1>To-do App</h1>
+
+        <form className="todo-item" onSubmit={handleSubmit}>
+          <input type="text" value={todo} onChange={(e) => setToDo(e.target.value)} />
+          <button className="action-button" type="submit">{(editTodoId !== null) ? "edit" : "add"}</button>
+        </form>
+
+        <ul className="tasks-container">
+          {todoList.map((item) => (
+          <li className='tasks-item' key={item.id}>
+            <span className="tasks-text" >{item.todo} </span>
+              <button className="action-button" onClick={() => handleDelete(item.id)}>delete</button>
+              <button className="action-button"  onClick={() => {
+                setEditToDo(item.id);
+                setToDo(item.todo);
+                }}>edit</button>
+            </li>
+          ))}
+        </ul>
+        
+      </div>
+      </div>
   );
 }
 
